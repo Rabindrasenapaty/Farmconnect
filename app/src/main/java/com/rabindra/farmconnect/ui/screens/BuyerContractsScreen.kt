5b42239ccbe4bee15330c1563e0f52d98699c49f
Baibhab Sahu <147395059+Baibhab13@users.noreply.github.com>
2025-01-07 13:41:24 +0530

    Baibhab - UI updated of the buyer screen final

--- app/src/main/java/com/rabindra/farmconnect/ui/screens/BuyerContractsScreen.kt
+++ app/src/main/java/com/rabindra/farmconnect/ui/screens/BuyerContractsScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
